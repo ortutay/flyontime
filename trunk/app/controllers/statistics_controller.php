78de3aedd5bfde38a91e4e9a98d9fbdb6f2a2750
josh.sulkin <josh.sulkin@08365dcc-5634-11de-8888-c79ed7c8b0c3>
2009-06-12 05:50:55 +0000

    Modified and added airline stats.

--- trunk/app/controllers/statistics_controller.php
+++ trunk/app/controllers/statistics_controller.php
@@ -251,7 +251,7 @@ class StatisticsController extends AppController {
 		
 		switch($query)
 		{
-			case 'departuredelays':
+			case 'percentontime':
 				
 				$airlines = $this->Log->find('all',
 					array(
@@ -259,29 +259,51 @@ class StatisticsController extends AppController {
 							'Log.Month',
 							'Log.Year',
 							'Log.UniqueCarrier',
-							'SUM(Log.DepDelay) as TotalMinutesDelayed'
-						),
-						'conditions' => array(
-							'Log.Cancelled' => 0,
-							'Log.Diverted' => 0
+							'((1-((SUM(Log.ArrDel15) + SUM(Log.Cancelled) + SUM(Log.Diverted))/COUNT(Log.UniqueCarrier)))*100) as PercentOnTime'
 						),
 						'group' => array(
 							'Log.UniqueCarrier'
 						),
 						'order' => array(
-							'TotalMinutesDelayed DESC'
+							'PercentOnTime DESC'
 						),
 						'limit' => 20
 					)
 				);
 
-				$this->set('Name', 'Airline Departure Delays');
-				$this->set('DataTitle', 'Minutes Delayed');
-				$this->set('DataValue', 'TotalMinutesDelayed');
+				$this->set('Name', 'Percent of Flights Arriving On Time');
+				$this->set('DataTitle', 'Percent On-Time');
+				$this->set('DataValue', 'PercentOnTime');
+				
+				break;
+				
+			case 'percentcancelled':
+				
+				$airlines = $this->Log->find('all',
+					array(
+						'fields' => array(
+							'Log.Month',
+							'Log.Year',
+							'Log.UniqueCarrier',
+							'((SUM(Log.Cancelled)/COUNT(Log.UniqueCarrier))*100) as PercentCancelled'
+						),
+						'group' => array(
+							'Log.UniqueCarrier'
+						),
+						'order' => array(
+							'PercentCancelled DESC'
+						),
+						'limit' => 20
+					)
+				);
+
+				$this->set('Name', 'Percent of Flights Cancelled');
+				$this->set('DataTitle', 'Percent Cancelled');
+				$this->set('DataValue', 'PercentCancelled');
 				
 				break;
 			
-			case 'cancelledflights':
+			case 'percentdiverted':
 				
 				$airlines = $this->Log->find('all',
 					array(
@@ -289,24 +311,73 @@ class StatisticsController extends AppController {
 							'Log.Month',
 							'Log.Year',
 							'Log.UniqueCarrier',
-							'COUNT(Log.Cancelled) as NumCancelledFlights'
-						),
-						'conditions' => array(
-							'Log.Cancelled' => 0
+							'((SUM(Log.Diverted)/COUNT(Log.UniqueCarrier))*100) as PercentDiverted'
 						),
 						'group' => array(
 							'Log.UniqueCarrier'
 						),
 						'order' => array(
-							'NumCancelledFlights DESC'
+							'PercentDiverted DESC'
 						),
 						'limit' => 20
 					)
 				);
 
-				$this->set('Name', 'Airline Cancelled Flights');
-				$this->set('DataTitle', 'Cancelled Flights');
-				$this->set('DataValue', 'NumCancelledFlights');
+				$this->set('Name', 'Percent of Flights Diverted');
+				$this->set('DataTitle', 'Percent Diverted');
+				$this->set('DataValue', 'PercentDiverted');
+				
+				break;
+				
+			case 'averagearrivaldelay':
+				
+				$airlines = $this->Log->find('all',
+					array(
+						'fields' => array(
+							'Log.Month',
+							'Log.Year',
+							'Log.UniqueCarrier',
+							'AVG(Log.ArrDelay) as AverageMinutesDelayed'
+						),
+						'group' => array(
+							'Log.UniqueCarrier'
+						),
+						'order' => array(
+							'AverageMinutesDelayed DESC'
+						),
+						'limit' => 20
+					)
+				);
+
+				$this->set('Name', 'Average Minutes Arriving Late');
+				$this->set('DataTitle', 'Average Minutes Late');
+				$this->set('DataValue', 'AverageMinutesDelayed');
+				
+				break;
+				
+			case 'scheduledflights':
+				
+				$airlines = $this->Log->find('all',
+					array(
+						'fields' => array(
+							'Log.Month',
+							'Log.Year',
+							'Log.UniqueCarrier',
+							'COUNT(Log.UniqueCarrier) as ScheduledFlights'
+						),
+						'group' => array(
+							'Log.UniqueCarrier'
+						),
+						'order' => array(
+							'ScheduledFlights DESC'
+						),
+						'limit' => 20
+					)
+				);
+
+				$this->set('Name', 'Number of Scheduled Flights');
+				$this->set('DataTitle', 'Scheduled Flights');
+				$this->set('DataValue', 'ScheduledFlights');
 				
 				break;
 				
